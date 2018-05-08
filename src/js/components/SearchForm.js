df327322969f538f1aab011e6357705c3646c21d
kensan73 <ken.60625@gmail.com>
2018-05-08 06:24:48 -0500

    step5 add form submit

--- src/js/components/SearchForm.js
+++ src/js/components/SearchForm.js
@@ -7,14 +7,20 @@ var SearchForm = React.createClass({
     return(
       <div className="search-form">
         <h1 className="text-center">Search for a movie</h1>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <input type="text" className="form-controller" ref="title" placeholder="Enter a movie title..." />
-            <button className="btn btn-primary btn-block">Search movies</button>
           </div>
+          <button className="btn btn-primary btn-block">Search movies</button>
         </form>
       </div>
     )
+  },
+
+  onSubmit: function(e){
+    e.preventDefault();
+
+    console.log(this.refs.title.value);
   }
 });
 
